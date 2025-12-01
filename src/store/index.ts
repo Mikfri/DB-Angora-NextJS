474c1d76560c0ae3abf32535d42a826efee7aafe
Mikkel Friborg <Mikk.fri@gmail.com>
2025-12-01 21:21:45 +0100

    BUG-FIX: • Fjernet gammel fil som importede andre gamle levn som allerede var slettede

--- src/store/index.ts
+++
@@ -1,8 +0,0 @@
-// src/store/index.ts
-/**
- * Central store exports fil
- * Eksporterer alle Zustand stores fra ét centralt sted 
- * for nem import i komponenterne
- */
-export * from './authStore';
-export * from './rabbitStore';
